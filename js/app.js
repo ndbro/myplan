469f95919b212df1bafd22b82c1bdcbab2a9fdf3
ndbro <nurmuhammadabdumutallibov@gmail.com>
2023-10-18 10:08:03 +0500

    my plan education

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let options = {
+  strings: ['My Plan', 'One step to future'],
+  typeSpeed: 60,
+  startDelay:500,
+  backSpeed: 50,
+  backDelay: 500,
+  loop: true,
+  loopCount: Infinity
+};
+
+let typed = new Typed('.element', options);
